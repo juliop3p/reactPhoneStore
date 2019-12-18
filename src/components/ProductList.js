916bef26d6770031cb6ce4e95cbe49e9f1bfcc61
juliop3p <julio15.zn@gmail.com>
2019-12-18 12:36:34 -0300

    inital commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Product from './Product'
+import Title from './Title'
+import {storeProducts} from '../data'
+import {ProductConsumer} from '../context'
+
+export default class ProductList extends Component {
+    state = {
+        products: storeProducts
+    }
+    
+    render() {
+        return (
+            <>
+                <div className="py-5">
+                    <div className="container">
+                            <Title name="our" title="products"/>
+                        <div className="row">
+                            <ProductConsumer>
+                                {data => {
+                                    return data.products.map(product => {
+                                        return <Product product={product} key={product.id}/>
+                                    })
+                                }}
+                            </ProductConsumer>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
